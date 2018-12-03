1952f58491a70c4f60f987e084e1e33363ef1818
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-12-03 14:26:57 -0500

    Made words flow next to each other

--- src/containers/WordContainer.js
+++ src/containers/WordContainer.js
@@ -13,17 +13,18 @@ export default class WordContainer extends React.Component {
     }
   }
 
-  componentDidMount(){
-    this.setState({
-      finishedWords: this.props.givenTextWords.map((word,index)=>{
-        debugger
-        return <NextWord word={word} key={`N${index}`} />
-      })
+  componentDidUpdate(){
+    const finishedWords = this.props.givenTextWords.map((word,index)=>{
+      return <NextWord word={word} key={`N${index}`} />
     })
+    if (this.state.finishedWords.length === 0){ // TODO: Refactor this incase props.givenTextWords changes
+      this.setState({finishedWords})
+    }
+
   }
 
   renderWords = () => {
-    return this.state.finishedWords.slice(0, this.props.index)
+    return this.state.finishedWords.slice(this.props.index, this.props.index+3)
   }
 
   render() {
